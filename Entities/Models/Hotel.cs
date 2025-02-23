29de726a42235f331afc432ade91e5690658be95
domag <domag@DMGJ>
2025-02-23 22:47:59 +0100

    Add project files.

---
+++ Entities/Models/Hotel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class Hotel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string PostalCode { get; set; }
+        public List<Room> Rooms { get; set; }
+    }
+}
---
+++ Entities/Models/Room.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public enum RoomType
+    {
+        Single,
+        Double,
+        Suite,
+        Deluxe,
+        Executive,
+        Presidential
+    }
+    public class Room
+    {
+        [Key]
+        public int Id { get; set; }
+        public int HotelId { get; set; }
+        public Hotel Hotel { get; set; }
+        public RoomType RoomType { get; set; } 
+        public decimal PricePerNight { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
