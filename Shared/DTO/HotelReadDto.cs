3ea53143aacd338c63c26454667d8be30f6e8192
dmgoj <domagoj.grgic@outlook.com>
2025-03-03 00:35:57 +0100

    feat: Add Shared project and Hotel DTOs for create, read and update

---
+++ Shared/DTO/HotelReadDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DTO
+{
+    public class HotelReadDto
+    {
+        public string Name { get; init; }
+        public string StreetAddress { get; init; }
+        public string City { get; init; }
+        public string Country { get; init; }
+        public string PostalCode { get; init; }
+    }
+}
---
+++ Shared/DTO/HotelUpdateDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DTO
+{
+    public record HotelUpdateDto
+    {
+        public string? Name { get; init; }
+        public string? StreetAddress { get; init; }
+        public string? City { get; init; }
+        public string? Country { get; init; }
+        public string? PostalCode { get; init; }
+    }
+
+
+}
