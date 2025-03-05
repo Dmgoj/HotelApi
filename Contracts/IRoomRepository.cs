15bb4af62ddceacaa04342d54057b17a08db52f4
dmgoj <domagoj.grgic@outlook.com>
2025-03-05 22:49:51 +0100

    feat: Add IRoomRepository and implement RoomRepository for basic CRUD functionality

---
+++ Contracts/IRoomRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Models;
+using Shared.Helpers;
+
+namespace Contracts
+{
+    public interface IRoomRepository
+    {
+        public Task<PaginatedList<Room>> GetAllRoomsAsync(int pageIndex, int pageSize, bool trackChanges);
+        public Task<Room> GetRoomAsync(int roomId, bool trackChanges);
+
+        public Task CreateRoomAsync(Room room);
+        public Task UpdateRoomAsync(Room room);
+        public Task DeleteRoomAsync(Room room);
+    }
+}
---
+++ Repository/RoomRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contracts;
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Shared.Helpers;
+
+namespace Repository
+{
+    public class RoomRepository : IRoomRepository
+    {
+        private readonly RepositoryContext _context;
+        public RoomRepository(RepositoryContext repositoryContext)
+        {
+            _context = repositoryContext;
+        }
+        public Task CreateRoomAsync(Room room)
+        {
+            _context.Rooms.Add(room);
+            return _context.SaveChangesAsync();
+        }
+
+        public Task DeleteRoomAsync(Room room)
+        {
+            _context.Rooms.Remove(room);
+            return _context.SaveChangesAsync();
+        }
+
+        public Task<PaginatedList<Room>> GetAllRoomsAsync(int pageIndex, int pageSize, bool trackChanges)
+        {
+            var query = trackChanges ? _context.Rooms : _context.Rooms.AsNoTracking();
+            return  PaginatedList<Room>.CreateAsync(_context.Rooms.AsQueryable(), pageIndex, pageSize);
+
+        }
+
+        public  Task<Room> GetRoomAsync(int roomId, bool trackChanges)
+        {
+            return  _context.Rooms
+           .AsTracking(trackChanges ? QueryTrackingBehavior.TrackAll : QueryTrackingBehavior.NoTracking)
+           .FirstOrDefaultAsync(h => h.Id == roomId);
+        }
+
+        public Task UpdateRoomAsync(Room room)
+        {
+            _context.Update(room);
+            return _context.SaveChangesAsync();
+        }
+    }
+}
