c8544f16eb3a41d86ed93c574629ac1ec9f5559e
dmgoj <domagoj.grgic@outlook.com>
2025-03-09 20:40:18 +0100

    refactor: Replace manual DTO mapping with Map extension in HotelService

--- Services.Contracts/IHotelService.cs
+++ Services.Contracts/IHotelService.cs
@@ -7,7 +7,7 @@ namespace Services.Contracts
     public interface IHotelService
     {
         Task<Hotel> CreateHotelAsync(HotelCreateDto hotel);
-        Task<Hotel> GetHotelAsync(int hotelId, bool trackChanges);
+        Task<HotelReadDto> GetHotelAsync(int hotelId, bool trackChanges);
         Task<PaginatedList<HotelReadDto>> GetAllHotelsAsync(int pageIndex, int pageSize, bool trackChanges); 
         Task <Hotel> UpdateHotelAsync(int id, HotelUpdateDto hotelDto);
         Task DeleteHotelAsync(int hotelId);
--- Services/HotelService.cs
+++ Services/HotelService.cs
@@ -1,4 +1,5 @@
 ﻿using Contracts;
+using Entities;
 using Entities.Models;
 using Repository;
 using Services.Contracts;
@@ -19,19 +20,10 @@ namespace Services
             if (hotelDto == null)
                 throw new ArgumentNullException(nameof(hotelDto)); 
 
-            if (string.IsNullOrWhiteSpace(hotelDto.Name))
-                throw new ArgumentException("Hotel name is required"); 
+            var hotel = new Hotel();
 
-            var hotel = new Hotel
-            {
-                Name = hotelDto.Name,
-                StreetAddress = hotelDto.StreetAddress,
-                City = hotelDto.City,
-                Country = hotelDto.Country,
-                PostalCode = hotelDto.PostalCode
-            };
+            hotelDto.Map(hotel);
 
-            
             await _hotelRepository.CreateHotelAsync(hotel);
 
             return hotel;
@@ -52,20 +44,17 @@ namespace Services
         public async Task<PaginatedList<HotelReadDto>> GetAllHotelsAsync(int pageIndex, int pageSize, bool trackChanges)
         {
             var hotels = await _hotelRepository.GetAllHotelsAsync(pageIndex, pageSize, trackChanges);
-
-            var hotelDtos = hotels.Select(hotel => new HotelReadDto
+            var hotelDtos = hotels.Select(hotel =>
             {
-                Name = hotel.Name,
-                StreetAddress = hotel.StreetAddress,
-                City = hotel.City,
-                Country = hotel.Country,
-                PostalCode = hotel.PostalCode
+                var hotelReadDto = new HotelReadDto();
+                hotel.Map(hotelReadDto); 
+                return hotelReadDto;
             }).ToList();
-          
+
             return new PaginatedList<HotelReadDto>(hotelDtos, hotels.Count, pageIndex, pageSize);
         }
 
-        public async Task<Hotel> GetHotelAsync(int hotelId, bool trackChanges)
+        public async Task<HotelReadDto> GetHotelAsync(int hotelId, bool trackChanges)
         {
             if (hotelId <= 0)
                 throw new ArgumentException("Hotel ID must be greater than zero.", nameof(hotelId));
@@ -73,7 +62,13 @@ namespace Services
             
             var hotel = await _hotelRepository.GetHotelAsync(hotelId, trackChanges);
 
-            return hotel ?? throw new KeyNotFoundException($"Hotel with ID {hotelId} not found.");
+            if (hotel == null)
+                throw new KeyNotFoundException($"Hotel with ID {hotelId} not found.");
+
+            var hotelReadDto = new HotelReadDto();
+            hotel.Map(hotelReadDto); 
+
+            return hotelReadDto;
         }
 
         public async Task<Hotel> UpdateHotelAsync(int id, HotelUpdateDto hotelDto)
@@ -85,18 +80,7 @@ namespace Services
                 throw new KeyNotFoundException($"Hotel with ID {id} not found.");
             }
 
-            foreach (var prop in typeof(HotelUpdateDto).GetProperties())
-            {
-                var newValue = prop.GetValue(hotelDto);
-                if (newValue is not null) 
-                {
-                    var hotelProp = typeof(Hotel).GetProperty(prop.Name);
-                    if (hotelProp is not null && hotelProp.CanWrite)
-                    {
-                        hotelProp.SetValue(hotel, newValue);
-                    }
-                }
-            }
+            hotelDto.Map(hotel);
 
             await _hotelRepository.UpdateHotelAsync(hotel);
 
