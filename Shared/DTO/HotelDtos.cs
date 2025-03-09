e726eff189ac3c1e175caae6929ffcee1ce3e593
dmgoj <domagoj.grgic@outlook.com>
2025-03-09 21:08:27 +0100

    feat: Add HotelDto with Create,Read and Update records, delete separate dto files

---
+++ Shared/DTO/HotelDtos.cs
@@ -0,0 +1,26 @@
+﻿public record HotelCreateDto
+{
+    public string Name { get; init; }
+    public string StreetAddress { get; init; }
+    public string City { get; init; }
+    public string Country { get; init; }
+    public string PostalCode { get; init; }
+}
+
+public record HotelReadDto
+{
+    public string Name { get; init; }
+    public string StreetAddress { get; init; }
+    public string City { get; init; }
+    public string Country { get; init; }
+    public string PostalCode { get; init; }
+}
+
+public record HotelUpdateDto
+{
+    public string? Name { get; init; }
+    public string? StreetAddress { get; init; }
+    public string? City { get; init; }
+    public string? Country { get; init; }
+    public string? PostalCode { get; init; }
+}
