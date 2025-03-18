eb1bf10dba533a5c45f7235472eda2896de42eac
dmgoj <domagoj.grgic@outlook.com>
2025-03-18 23:14:43 +0100

    feat: Add new ReservationGuest model

---
+++ Entities/Models/ReservationGuest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class ReservationGuest
+    {
+        public int ReservationId { get; set; }
+        public Reservation Reservation { get; set; }
+
+        public int GuestId { get; set; }
+        public Guest Guest { get; set; }
+    }
+}
