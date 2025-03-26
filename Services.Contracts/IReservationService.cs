5e14d117786b310423dfbdc7398adf312cdc303d
dmgoj <domagoj.grgic@outlook.com>
2025-03-26 23:42:48 +0100

    feat: Add IReservationService

---
+++ Services.Contracts/IReservationService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Contracts
+{
+    public interface IReservationService
+    {
+        Task<RoomReadDto> CreateReservationAsync(ReservationCreateDto reservationDto);
+        Task<RoomReadDto> GetReservationAsync(int reservationId, bool trackChanges);
+        Task<IEnumerable<RoomReadDto>> GetReservationsByRoomAsync(int roomId, bool trackChanges);
+        Task<bool> UpdateReservationAsync(int reservationId, ReservationUpdateDto reservationDto);
+        Task<bool> DeleteReservationAsync(int reservationId);
+    }
+}
