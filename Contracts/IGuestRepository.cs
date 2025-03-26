d19fff5b6cccab86ff1bdab000277643d7dcd985
dmgoj <domagoj.grgic@outlook.com>
2025-03-26 23:41:49 +0100

    feat: Add IGuestRepository

---
+++ Contracts/IGuestRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Models;
+
+namespace Contracts
+{
+   public interface IGuestRepository
+{
+    Task<Guest?> GetGuestByUniqueIdentifiersAsync(string email, string phoneNumber, string pin);
+    Task AddGuestAsync(Guest guest);
+}
+}
