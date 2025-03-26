214eca4826f42710405e63b5303245d05ea00b26
dmgoj <domagoj.grgic@outlook.com>
2025-03-26 23:42:17 +0100

    feat: Implement GuestRepository

---
+++ Repository/GuestRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contracts;
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Repository;
+
+public class GuestRepository : IGuestRepository
+{
+    private readonly RepositoryContext _context;
+
+    public GuestRepository(RepositoryContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Guest?> GetGuestByUniqueIdentifiersAsync(string email, string phoneNumber, string pin)
+    {
+        return await _context.Guests
+            .FirstOrDefaultAsync(g =>
+                g.Email == email || g.PhoneNumber == phoneNumber || g.PIN == pin);
+    }
+
+    public async Task AddGuestAsync(Guest guest)
+    {
+        _context.Guests.Add(guest);
+        await _context.SaveChangesAsync();
+    }
+}
