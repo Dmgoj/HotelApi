37303851a4806f940391bf6929291e2f0fdda304
dmgoj <domagoj.grgic@outlook.com>
2025-03-27 23:58:01 +0100

    feat: Add Guest and Reservation DTOs

---
+++ Shared/DTO/GuestDtos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DTO
+{
+    public record GuestCreateDto
+    {
+        public string PIN { get; init; }
+        public string FirstName { get; init; }
+        public string LastName { get; init; }
+        public string Email { get; init; }
+        public string PhoneNumber { get; init; }
+    }
+
+    public record GuestReadDto
+    {
+        public string PIN { get; init; }
+        public string FirstName { get; init; }
+        public string LastName { get; init; }
+        public string Email { get; init; }
+        public string PhoneNumber { get; init; }
+    }
+}
---
+++ Shared/DTO/ReservationDtos.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json.Serialization;
+using Entities.Models;
+using Shared.DTO;
+
+public record ReservationCreateDto
+{
+    public int RoomId { get; set; }
+    public int HotelId { get; set; }
+    public DateTime CheckInDate { get; set; }
+    public DateTime CheckOutDate { get; set; }
+    public decimal TotalPrice { get; set; }
+    public ReservationStatus ReservationStatus { get; set; }
+    public List<GuestCreateDto> Guests { get; set; } = new();
+}
+
+public record ReservationReadDto
+{
+    [JsonPropertyName("Room Number")]
+    public int RoomId { get; init; }
+    public string HotelName { get; init; }
+    public DateTime CheckInDate { get; init; }
+    public DateTime CheckOutDate { get; init; }
+    public decimal TotalPrice { get; init; }
+    public ReservationStatus ReservationStatus { get; init; }
+    public List<GuestReadDto> Guests { get; init; } = new();
+}
+
+public record ReservationUpdateDto
+{
+    public int RoomId { get; set; }
+    public int HotelId { get; set; }
+    public DateTime CheckInDate { get; set; }
+    public DateTime CheckOutDate { get; set; }
+    public decimal TotalPrice { get; set; }
+    public ReservationStatus ReservationStatus { get; set; }
+}
